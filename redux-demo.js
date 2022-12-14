c8b07981852072613d1bcbded482b42e7904122c
tadams95 <tyrelleadams28@gmail.com>
2022-12-13 17:18:04 -0800

    First Commit

---
+++ redux-demo.js
@@ -0,0 +1,20 @@
+const redux = require("redux");
+// import { createStore } from 'redux'
+var createStore = require("redux").createStore;
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
